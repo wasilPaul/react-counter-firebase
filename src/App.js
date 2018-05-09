bf1bd77ae8ea7c43ee341926669011c5365b05ff
Pawel Wasil <wasil.paul@gmail.com>
2018-05-09 11:53:32 +0200

    Counter finish

--- src/App.js
+++ src/App.js
@@ -1,21 +1,52 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+
 
 class App extends Component {
+  state = {
+    counter: 0,
+  }
+
+  componentDidMount() {
+    fetch('https://jfddl4-sandbox.firebaseio.com/pwasil/counter/.json')
+      .then(re => re.json())
+      .then(actualCounterValFromDb => this.setState({ counter: actualCounterValFromDb }))
+  }
+
+  decHandler = () => this.setState({ counter: this.state.counter + 1 }, this.saveToDb)
+  incHandler = () => this.setState({ counter: this.state.counter - 1 }, this.saveToDb)
+
+  saveToDb = () => fetch(
+    'https://jfddl4-sandbox.firebaseio.com/pwasil/counter/.json',
+    {
+      method: 'PUT',
+      body: JSON.stringify(this.state.counter)
+    })
+
+  readFromDb = () => fetch(
+    'https://jfddl4-sandbox.firebaseio.com/pwasil/counter/.json',
+    {
+      method: 'PUT',
+      body: JSON.stringify(this.state.counter)
+    })
+
+
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div >
+        <h1>{this.state.counter}</h1>
+        <button onClick={this.incHandler}>-</button>
+        <button onClick={this.decHandler}>+</button>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+
+
+export default App
+
+
+
+
