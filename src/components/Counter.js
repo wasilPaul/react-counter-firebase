2995b08653d9c5d9fd19c97e449c87a5107521eb
Pawel Wasil <wasil.paul@gmail.com>
2018-05-10 11:53:03 +0200

    saving changes to database

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,33 +2,37 @@ import React from 'react'
 import { database } from '../firebase'
 
 class Counter extends React.Component {
-    state = {
-        counter: 0
-    }
+  state = {
+    counter: 0
+  }
 
-    componentDidMount() {
-        database.ref('/counter')    // metoda firebase zwraca referencje do danego miejsca
-            .on(                      //once nasluchiwanie jednorazowe czy zmieniaa sie stan countera
-                'value',                //jesli zmieni sie valu wyknaj funkcje
-                (snapshot) => {
-                    this.setState({
-                        counter: snapshot.val()
-                    })})
+  componentDidMount() {
+    database.ref('/counter')    // metoda firebase zwraca referencje do danego miejsca
+      .on(                      //once nasluchiwanie jednorazowe czy zmieniaa sie stan countera
+        'value',                //jesli zmieni sie valu wyknaj funkcje
+        (snapshot) => {
+          this.setState({
+            counter: snapshot.val()
+          })
+        }
+      )
+  }
 
-    }
+  saveToOb = (data) => {
+    database.ref('/counter').set(data)
+  }
 
+  render() {
 
-    render() {
+    return (
 
-        return (
-
-            <div>
-                <h1> {this.state.counter}</h1>
-                <button onClick={this.oddFunction}> - </button>
-                <button onClick={this.addFunction}> + </button>
-            </div >
-        )
-    }
+      <div>
+        <h1> {this.state.counter}</h1>
+        <button onClick={() => this.saveToOb(this.state.counter - 1)}> - </button>
+        <button onClick={() => this.saveToOb(this.state.counter + 1)}> + </button>
+      </div >
+    )
+  }
 
 }
 
